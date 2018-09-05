4be99bb141798bd11c897ccdfae7104475db6fc6
hihozhou <hihozhou@gmail.com>
2018-09-05 19:34:34 +0800

    容器

---
+++ src/Container.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiho
+ * Date: 18-9-5
+ * Time: 下午5:45
+ */
+namespace HihoZhou\Hertz;
+
+/**
+ * 一个依赖注入的容器
+ * Class Container
+ */
+class Container
+{
+
+    use Singleton;
+
+    protected $values = [];
+
+
+    public function set(string $key, $value)
+    {
+        $this->values[$key] = $value;
+    }
+
+
+    public function get($key)
+    {
+        if (array_keys($this->values)) {
+            return $this->values[$key];
+        }
+        return null;
+    }
+
+
+    /**
+     * Returns all defined value names.
+     *
+     * @return array An array of value names
+     */
+    public function keys()
+    {
+        return array_keys($this->values);
+    }
+}
